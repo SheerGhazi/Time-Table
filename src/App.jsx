e806759bde31eb2fcb2d1418cfa4e03b78bfff0b
SheerGhazi <sher.ghazi2192@gmail.com>
2024-09-17 22:13:58 +0500

    feat: React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Pages/Home'
-import { Routes, Route, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import TimeTable from './Components/TimeTable/TimeTable'
 import TTable from './Pages/TTable'
 
@@ -13,7 +13,13 @@ function App() {
   return (
     <>
 
-    
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}  />
+          <Route path='/table' element={<TTable/>}  />
+        </Routes>
+      </Router>
+
     {/* <Home/>
     <TTable/> */}
     </>
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import bannerImage from "../../assets/banner-bg.png";
+import { Link } from 'react-router-dom';
 
 
 const Banner = () => {
@@ -22,7 +23,7 @@ const Banner = () => {
             New Table is issued for CS Department. Students click below to see time table
           </p>
         </div>
-        <a className="" href="/table">
+        <Link className="" to="/table">
           <button className="flex justify-center items-center gap-2 bg-blue-700 py-4 px-9 rounded-md hover:bg-blue-950">
             <span className="invert">
            
@@ -32,7 +33,7 @@ const Banner = () => {
               Time Table
             </p>
           </button>
-        </a>
+        </Link>
       </div>
 
      </div>
